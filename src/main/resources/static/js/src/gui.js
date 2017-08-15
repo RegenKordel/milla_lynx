d93b3eb9169efb106151b97576f571d8fd662286
Iivo Raitahila <iivo.raitahila@helsinki.fi>
2017-08-15 16:08:38 +0300

    added attributes to GUI

--- src/main/resources/static/js/src/gui.js
+++ src/main/resources/static/js/src/gui.js
@@ -1,3 +1,7 @@
+/**
+ * Send model or model request to Milla
+ * @returns
+ */
 function postToMilla() {
 	var xhttp = new XMLHttpRequest(); 
 	xhttp.onreadystatechange = function() {
@@ -12,6 +16,10 @@ function postToMilla() {
 	xhttp.send(document.getElementById("payload").value);
 }
 
+/**
+ * Fetches a model from Mulperi, i.e. the selectable features
+ * @returns
+ */
 function getOptions() {
 	var url = document.getElementById("mulperiUrl").value
 		+ "/models/" + document.getElementById("modelName").value;
@@ -30,6 +38,10 @@ function getOptions() {
 	xhttp.send("[]");
 }
 
+/**
+ * Finds a valid configuration with CaaS (via Mulperi)
+ * @returns
+ */
 function getConfiguration() {
 	var url = document.getElementById("mulperiUrl").value
 		+ "/models/" + document.getElementById("modelName").value + "/configurations";
@@ -46,7 +58,7 @@ function getConfiguration() {
 	
 	xhttp.open("POST", url, true);
 	xhttp.setRequestHeader("Content-type", "application/json");
-	xhttp.send(JSON.stringify(selectionsToJson()));
+	xhttp.send(selectionsToJson());
 }
 
 /**
@@ -66,12 +78,7 @@ function createSelectionTree(parent, features) {
 		var checkbox = document.createElement("input");
 		checkbox.setAttribute("id", "checkbox_" + type);
 		checkbox.setAttribute("type", "checkbox");
-		checkbox.onchange = function(){ 
-			var e = document.getElementById("autosubmit"); //check for autosubmit
-			if(e.options[e.selectedIndex].value === "1") {
-				getConfiguration();
-			}
-		};
+		checkbox.onchange = function(){ autosubmit(); };
 		
 		var element = document.createElement("div");
 		element.appendChild(checkbox);
@@ -80,10 +87,47 @@ function createSelectionTree(parent, features) {
 	    element.setAttribute("class", "level");
 	    document.getElementById(parent).appendChild(element);
 	    
+	    var attributes = {};
+	    for (var j = 0; j < feature["attributes"].length; j++) { //dropdown for each attribute
+	    	var attribute = document.createElement("select");
+	    	attribute.setAttribute("id", "select_" + type + "-" + feature["attributes"][j]["name"]);
+	    	attribute.onchange = function(){ autosubmit(); };
+	    	attributes[ feature["attributes"][j]["name"] ] = attribute;
+	    	
+		}
+	    
+	    for (var j = 0; j < feature["attributes"].length; j++) { //values for each attribute
+	    	var attribute = attributes[ feature["attributes"][j]["name"] ];
+	    	attribute.options.add( new Option(feature["attributes"][j]["value"], feature["attributes"][j]["value"]) );
+		}
+	    
+	    for (var key in attributes) {
+	    	var attributeDiv = document.createElement("div");
+	    	attributeDiv.appendChild(document.createTextNode(key + ": "));
+	    	attributeDiv.appendChild(attributes[key]);
+	    	attributeDiv.setAttribute("class", "level");
+	    	element.appendChild(attributeDiv);
+	    }
+	    
 	    createSelectionTree(name + type, feature["features"]);
 	}
 }
 
+/**
+ * If autosubmit is selected, submit form on selection change
+ * @returns
+ */
+function autosubmit() {
+	var e = document.getElementById("autosubmit");
+	if(e.options[e.selectedIndex].value === "1") {
+		getConfiguration();
+	}
+}
+
+/**
+ * Deselect all checkboxes
+ * @returns
+ */
 function deselectFeatures() {
 	var checkboxes = document.querySelectorAll("input[type='checkbox']");
 	for (var i=0; i<checkboxes.length; i++) {
@@ -91,29 +135,61 @@ function deselectFeatures() {
 	}
 }
 
+/**
+ * Marks certain checkboxes as checked after configuration
+ * @param parent
+ * @param features
+ * @returns
+ */
 function selectFeatures(parent, features) {
 	for (var i = 0; i < features.length; i++){
 		var feature = features[i];
 		var name = feature["name"];
 		var type = feature["type"];
 		
+		//select feature
 		var checkbox = document.getElementById("checkbox_" + type);
 		checkbox.checked = true;
+		
+		//select attribute values
+		var attributes = feature["attributes"];
+		for(var j = 0; j < attributes.length; j++) {
+			var element = document.getElementById("select_" + type + "-" + attributes[j]["name"]);
+			element.value = attributes[j]["value"];
+		}
+		
 		
 	    selectFeatures(name, feature["features"]);
 	}
 }
 
+/**
+ * Serialises selected features as JSON
+ * @returns
+ */
 function selectionsToJson() {
 	var checkboxes = document.querySelectorAll("input[type='checkbox']");
 	var featureSelections = [];
 	
-	for (var i=0; i<checkboxes.length; i++) {
+	for (var i = 0; i < checkboxes.length; i++) {
 		if (checkboxes[i].checked) {
-			var selection = { "type":checkboxes[i].getAttribute("id").replace("checkbox_", "") };
+			//select feature
+			var type = checkboxes[i].getAttribute("id").replace("checkbox_", "");
+			var selection = { "type": type };
+			
+			//select attributes
+			var attributes = document.querySelectorAll("select[id^='select_" + type + "-']");
+			var selectedAttributes = [];
+			for(var j = 0; j < attributes.length; j++) {
+				var attributeName = attributes[j].id.replace("select_" + type + "-", "");
+				var attributeValue = attributes[j].options[attributes[j].selectedIndex].value;
+				selectedAttributes.push( { "name": attributeName, "value": attributeValue } );
+			}
+			selection["attributes"] = selectedAttributes;
+			
 			featureSelections.push(selection);
 		}
 	}
 	
-	return featureSelections;
+	return JSON.stringify(featureSelections);
 }
