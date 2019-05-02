78171a196269f9b17e0a951ba58a045213fb52db
laurtom <tlaurinen@gmail.com>
2019-05-02 11:55:58 +0300

    config for master

--- src/main/java/eu/openreq/milla/SwaggerConfig.java
+++ src/main/java/eu/openreq/milla/SwaggerConfig.java
@@ -17,6 +17,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+        		.host("api.openreq.eu/milla")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.openreq.milla.controllers"))    
                 .paths(PathSelectors.any())
