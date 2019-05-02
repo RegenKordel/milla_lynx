73f6e8e3f241d3fa8bcaee632c275fb6ccebc737
laurtom <tlaurinen@gmail.com>
2019-05-02 11:25:12 +0300

    properties fixed

--- src/main/java/eu/openreq/milla/SwaggerConfig.java
+++ src/main/java/eu/openreq/milla/SwaggerConfig.java
@@ -17,7 +17,6 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-        		.host("api.openreq.eu/milla")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.openreq.milla.controllers"))    
                 .paths(PathSelectors.any())
