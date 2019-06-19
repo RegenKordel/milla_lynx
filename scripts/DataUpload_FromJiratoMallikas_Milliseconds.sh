303cb63128eb9631224bff598f11546e05e6b793
laurtom <tlaurinen@gmail.com>
2019-06-19 16:55:48 +0300

    updated scripts

--- scripts/DataUpload_FromJiratoMallikas_Milliseconds.sh
+++ scripts/DataUpload_FromJiratoMallikas_Milliseconds.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 projects=("QTPLAYGROUND"  "QTWB"  "QTSOLBUG"  "QTSYSADM"  "QTJIRA"  "QSR"  "QDS"  "QTVSADDINBUG"  "QTWEBSITE"  "AUTOSUITE" "PYSIDE"  "QTCOMPONENTS"  "QTIFW"  "QBS"  "QTMOBILITY"  "QTQAINFRA"  "QT3DS" "QTCREATORBUG" "QTBUG")
 
-URL=localhost
+URL=https://api.openreq.eu/milla
 #URL="217.172.12.199"
 
 
@@ -11,7 +11,7 @@ for i in "${projects[@]}"
 do
 	echo -e "\nUploading $i from Jira to Mallikas"
 	start=`date +%s%N`
-	curl -X POST --header 'Content-Type: application/json' --header 'Accept: text/plain' -d $i 'http://'$URL':9203/qtJira' 
+	curl -k -X POST --header 'Accept: text/plain' $URL'/qtJira?projectId='$i 
 	end=`date +%s%N`
 	echo -e "\nRuntime = $(((end-start)/1000000)) milliseconds"
 done
--- scripts/DataUpload_FromMallikasToMulperi_Milliseconds.sh
+++ scripts/DataUpload_FromMallikasToMulperi_Milliseconds.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # author Mikko Raatikainen 
 
 ## Potential projects roughly in the order of size.
 projects=("QTPLAYGROUND"  "QTWB"  "QTSOLBUG"  "QTSYSADM"  "QTJIRA"  "QSR"  "QDS"  "QTVSADDINBUG"  "QTWEBSITE"  "AUTOSUITE" "PYSIDE"  "QTCOMPONENTS"  "QTIFW"  "QBS"  "QTMOBILITY"  "QTQAINFRA"  "QT3DS" "QTCREATORBUG" "QTBUG")
 
-URL=localhost
+URL=https://api.openreq.eu/milla
 #URL="217.172.12.199"
 
 
@@ -14,7 +14,7 @@ for i in "${projects[@]}"
 do
 	echo -e "\nPosting $i to Mulperi and Kelju to construct a transitive closure. "	
 	start=`date +%s%N`
-	curl -s -X POST --header 'Content-Type: application/json' --header 'Accept: text/plain'  'http://'$URL':9203/sendProjectToMulperi?projectId='$i
+	curl -k -X POST --header 'Accept: text/plain'  $URL'/sendProjectToMulperi?projectId='$i
 	end=`date +%s%N`
 	echo -e "\nRuntime = $(((end-start)/1000000)) milliseconds"
 
