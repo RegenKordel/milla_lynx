944966b7b1aa595269fa9d85b478ed58f0d17a70
laurtom <tlaurinen@gmail.com>
2019-08-07 20:34:37 +0300

    conversion change cancelled

--- src/main/java/eu/openreq/milla/services/MallikasService.java
+++ src/main/java/eu/openreq/milla/services/MallikasService.java
@@ -12,8 +12,8 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
@@ -156,11 +156,7 @@ public class MallikasService {
 	}
 	
 	public String convertAndUpdateDependencies(String dependencies, Boolean proposed, Boolean userInput) {
-	
 		Collection<Dependency> convertedDependencies = parseStringToDependencies(dependencies);
-		if (convertedDependencies==null) {
-			return "No dependencies found";
-		}
 		return updateDependencies(convertedDependencies, proposed, userInput);
 	}
 	
@@ -191,12 +187,10 @@ public class MallikasService {
 		Gson gson = new Gson();
 		JsonParser parser = new JsonParser();
 		JsonElement dependencyElement = parser.parse(dependencies);
-		JsonObject dependenciesJSON = dependencyElement.getAsJsonObject();
+		JsonArray dependenciesJSON = dependencyElement.getAsJsonArray();
 		Type listType = new TypeToken<List<Dependency>>(){}.getType();
 		
-		JsonElement element = dependenciesJSON.get("dependencies");
-		
-		Collection<Dependency> updatedDependencies = gson.fromJson(element, listType);
+		Collection<Dependency> updatedDependencies = gson.fromJson(dependenciesJSON, listType);
 		
 		return updatedDependencies;
 	}
